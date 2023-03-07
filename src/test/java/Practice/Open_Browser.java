0e57b5ece1eabdefc896179c1b9d301bb859419c
Chetan-S1404 <chetansanchali@gmail.com>
2023-03-07 17:27:07 +0530

    FirstCommit

---
+++ src/test/java/Practice/Open_Browser.java
@@ -0,0 +1,18 @@
+package Practice;
+
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class Open_Browser {
+
+	public static void browser() {
+		String key = "webdriver.chrome.driver";
+		String value = "C:\\Users\\chetan\\eclipse-workspace\\framework\\src\\main\\resources\\chromedriver.exe";
+		System.setProperty(key, value);
+		ChromeDriver driver=new ChromeDriver();
+		driver.get("https://www.cricbuzz.com/");
+
+	}
+
+	
+
+}
